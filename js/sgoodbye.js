d30cddfcfa0e5145ce8ba19456332df22ff831c4
Dk-hack <66866050+Dk-hack@users.noreply.github.com>
2020-08-26 23:34:10 +0530

    Add files via upload

---
+++ js/sgoodbye.js
@@ -0,0 +1,8 @@
+(function(window){
+    var byeSpeaker={};
+    var speakword = "Good Bye";
+    byeSpeaker.speak=function speak(name) {
+      console.log(speakword + " " + name);
+ };
+    window.byeSpeaker=byeSpeaker;
+})(window);
