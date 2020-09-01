5df25b56935f5800a2a716292b383fb1dea9e7b7
Dk-hack <66866050+Dk-hack@users.noreply.github.com>
2020-09-01 21:24:12 +0530

    Delete shello.js

--- js/shello.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-    var helloSpeaker={};
-    var speakword="Hello";
-    helloSpeaker.speak=function speak(name) {
-  console.log(speakword + " " + name);
-  };
-    window.helloSpeaker=helloSpeaker;
-    
-})(window);
