762716463f238035462de3e20f702050bb867701
Dk-hack <66866050+Dk-hack@users.noreply.github.com>
2020-09-01 21:23:59 +0530

    Delete sgoodbye.js

--- js/sgoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var byeSpeaker={};
-    var speakword = "Good Bye";
-    byeSpeaker.speak=function speak(name) {
-      console.log(speakword + " " + name);
- };
-    window.byeSpeaker=byeSpeaker;
-})(window);
